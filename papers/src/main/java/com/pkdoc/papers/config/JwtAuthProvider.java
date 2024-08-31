906d1bdf8a109bd1cc2d805bab2115b38650eccc
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-08-31 11:03:57 +0200

    [back] Add missing JwtAuthProvider file

---
+++ papers/src/main/java/com/pkdoc/papers/config/JwtAuthProvider.java
@@ -0,0 +1,56 @@
+package com.pkdoc.papers.config;
+
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.pkdoc.papers.DTOs.UserDTO;
+import com.pkdoc.papers.service.UserService;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class JwtAuthProvider {
+
+    @Value("${security.jwt.token.secret-key:secret-key}")
+    private String secretKey;
+    private final UserService userService;
+
+    @Autowired
+    public JwtAuthProvider(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username) {
+        Date now = new Date();
+        Date expires = new Date(now.getTime() + 3600000); // 1h
+
+        return JWT.create().withSubject(username).withIssuedAt(now).withExpiresAt(expires)
+                .sign(Algorithm.HMAC256(secretKey));
+    }
+
+    public Authentication validateToken(String token) {
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
+
+        DecodedJWT decoded = verifier.verify(token);
+        Optional<UserDTO> user = userService.findByEmail(decoded.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+    }
+}
