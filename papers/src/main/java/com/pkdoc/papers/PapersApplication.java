562eb002710b761e341d261b0cf7bec348697e5e
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-12-14 18:02:08 +0100

    [back] - Enable CORS from all origins

--- papers/src/main/java/com/pkdoc/papers/PapersApplication.java
+++ papers/src/main/java/com/pkdoc/papers/PapersApplication.java
@@ -2,11 +2,23 @@ package com.pkdoc.papers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PapersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PapersApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(PapersApplication.class, args);
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
